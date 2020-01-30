aaf541df0bf732da5aa1e6d707131e71c0ae3458
Robert Schneider <robmschn@gmail.com>
2020-01-30 16:05:50 -0500

    no output when I run the code

--- oci_toggle.py
+++ oci_toggle.py
@@ -1,2 +1,13 @@
 import oci
+import creds
 
+config = {
+    "user": creds.user_ocid,
+    "key_file": creds.key_file,
+    "fingerprint": creds.fingerprint,
+    "tenancy": creds.tenancy,
+    "pass_phrase": creds.pass_phrase,
+    "region": creds.region
+}
+
+oci.config.validate_config(config)
