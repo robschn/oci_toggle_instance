0a2d3d085baa61fc3ca42a8e2e9eaf5f637678dd
Robert Schneider <robmschn@gmail.com>
2020-01-31 14:59:17 -0500

    moved config file back to default location

--- oci_toggle.py
+++ oci_toggle.py
@@ -1,13 +1,12 @@
 import sys
 import oci
-import creds
 
 # make sure user is providing instance action
 # if len(sys.argv) < 2:
 #     print("No instance action provided. Please add 'start' or 'stop' after the script.")
 #     exit()
 
-config = oci.config.from_file(file_location= "config")
+config = oci.config.from_file()
 
 # set compute resource
 base_compute = oci.core.ComputeClient(config)
