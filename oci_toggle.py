315023ddabff410e419cf1c1f906b11d422d26cf
Robert Schneider <robmschn@gmail.com>
2020-01-31 15:12:46 -0500

    uncommented out somethings

--- oci_toggle.py
+++ oci_toggle.py
@@ -2,9 +2,9 @@ import sys
 import oci
 
 # make sure user is providing instance action
-# if len(sys.argv) < 2:
-#     print("No instance action provided. Please add 'start' or 'stop' after the script.")
-#     exit()
+if len(sys.argv) < 2:
+    print("No instance action provided. Please add 'start' or 'stop' after the script.")
+    exit()
 
 config = oci.config.from_file()
 
@@ -12,17 +12,17 @@ config = oci.config.from_file()
 base_compute = oci.core.ComputeClient(config)
 
 # get instance list
-instance_list = base_compute.list_instances(config["compartment_id"]).data
+instance_list = base_compute.list_instances(config['compartment_id']).data
 
 
 for instance in instance_list:
     # filter out any non Test Server types
     try:
         if instance.freeform_tags['Test'] == 'Shutdown':
-            print(instance.display_name, instance.freeform_tags)
+
             # send command passed through
-            # base_compute.instance_action(instance.id, f'{sys.argv[1]}')
-            # print(f'Sending {sys.argv[1]} command to {instance.display_name}...')
+            base_compute.instance_action(instance.id, f'{sys.argv[1]}')
+            print(f'Sending {sys.argv[1]} command to {instance.display_name}...')
 
     except KeyError:
         pass
