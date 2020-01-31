13e66081e739e23ce068bd8ec4ba925aa589d6d8
Robert Schneider <robmschn@gmail.com>
2020-01-31 14:26:37 -0500

    added sys.argvs to specify command

--- oci_toggle.py
+++ oci_toggle.py
@@ -1,5 +1,11 @@
+import sys
 import oci
 import creds
+
+# make sure user is providing instance action
+# if len(sys.argv) < 2:
+#     print("No instance action provided. Please add 'start' or 'stop' after the script.")
+#     exit()
 
 config = oci.config.from_file()
 
@@ -9,15 +15,20 @@ base_compute = oci.core.ComputeClient(config)
 # get instance list
 instance_list = base_compute.list_instances(creds.compartment_id).data
 
-test_instances = []
 
 for instance in instance_list:
-    # filter out any non TestServer lists
-    if instance.freeform_tags == {'ServerType' : 'Test'}:
-        test_instances.append(instance.id)
+    try:
+        # filter out any non Test Server types
+        if instance.freeform_tags['Test'] == 'Shutdown':
+            print(instance.display_name, instance.freeform_tags)
+            # send command passed through
+            # base_compute.instance_action(instance.id, f'{sys.argv[1]}')
+            # print(f'Sending {sys.argv[1]} command to {instance.display_name}...')
+
+    except KeyError:
+        pass
 
 # get instance_id of all instances with "Nightly/Weekend Shutdown" tag
-print(test_instances)
 
 # shutdown the instance state
 
