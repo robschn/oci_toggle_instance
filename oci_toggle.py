b9365bdf07aa4e371ad116f35c7f2f9edc4d469b
Robert Schneider <robmschn@gmail.com>
2020-01-31 10:25:17 -0500

    added config from file

--- oci_toggle.py
+++ oci_toggle.py
@@ -1,25 +1,10 @@
 import oci
 import creds
 
-config = {
-    "user": creds.user_ocid,
-    "key_file": creds.key_file,
-    "fingerprint": creds.fingerprint,
-    "tenancy": creds.tenancy,
-    "pass_phrase": creds.pass_phrase,
-    "region": creds.region
-}
+config = oci.config.from_file()
 
-# set identity
-identity = oci.identity.IdentityClient(config)
-
-# set user
-user = identity.get_user(config["user"]).data
-
-# set base computer
+# set compute and region
 base_compute = oci.core.compute_client.ComputeClient(config)
-
-# set region
 base_compute.base_client.set_region(config["region"])
 
 # get instance list
