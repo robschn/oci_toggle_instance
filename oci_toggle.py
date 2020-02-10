de480da15968677798b35c86af9eda99d46e55ea
Robert Schneider <robmschn@gmail.com>
2020-02-10 10:14:50 -0500

    formatted spacing

--- oci_toggle.py
+++ oci_toggle.py
@@ -2,19 +2,14 @@ import sys
 import oci
 
 def toggle_instance(state):
-   
-    # load config
-    config = oci.config.from_file()
-    # set compute resource
-    base_compute = oci.core.ComputeClient(config)
-    # get instance list
-    instance_list = base_compute.list_instances(config['compartment_id']).data
 
+    config = oci.config.from_file()
+    base_compute = oci.core.ComputeClient(config)
+
+    instance_list = base_compute.list_instances(config['compartment_id']).data
     for instance in instance_list:
-        # filter out any non Test Server types
         try:
             if instance.freeform_tags['Test'] == 'Shutdown':
-                # send command passed through
                 base_compute.instance_action(instance.id, state)
                 print(f'Sending {state.upper()} command to {instance.display_name}...')
         except KeyError:
